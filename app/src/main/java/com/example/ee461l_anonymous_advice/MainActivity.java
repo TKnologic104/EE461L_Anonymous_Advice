39c0be1af36adbf0d4bf22999a29acea6ebe091a
Tarang Khandpur <TKnologic104@gmail.com>
2017-03-13 01:17:10 -0500

    Need to Fix: Sign Out Button, Image, Landing Page EditText moves.

--- app/src/main/java/com/example/ee461l_anonymous_advice/MainActivity.java
+++ app/src/main/java/com/example/ee461l_anonymous_advice/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.ee461l_anonymous_advice;
 
 import android.content.Intent;
-import android.net.Uri;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -12,13 +11,12 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-//import com.example.tk_whatsappclone.R;
 import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
+
+//import com.example.tk_whatsappclone.R;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
