5e2a2dc5da059ece372e2dbff279d76ef40e2941
Karime Saad <karime.saad@utexas.edu>
2017-03-26 22:12:38 -0500

    Fixed RateAdvice and ReportReasons activites

--- app/src/main/java/com/example/ee461l_anonymous_advice/ReportReasons.java
+++ app/src/main/java/com/example/ee461l_anonymous_advice/ReportReasons.java
@@ -2,16 +2,17 @@ package com.example.ee461l_anonymous_advice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 
 public class ReportReasons extends AppCompatActivity {
 
-    private CheckBox reason1;
-    private CheckBox reason2;
-    private CheckBox reason3;
-    private CheckBox reason4;
-    private CheckBox reason5;
+    private CheckBox harrasment;
+    private CheckBox spam;
+    private CheckBox notAdvice;
+    private CheckBox joke;
+    private CheckBox offensive;
     private Button submitButton;
 
     @Override
@@ -19,13 +20,40 @@ public class ReportReasons extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_reasons);
 
-        reason1 = (CheckBox) findViewById(R.id.reason_harrasment);
-        reason2 = (CheckBox) findViewById(R.id.reason_spam);
-        reason3 = (CheckBox) findViewById(R.id.reason_notadvice);
-        reason4 = (CheckBox) findViewById(R.id.reason_joke);
-        reason5 = (CheckBox) findViewById(R.id.reason_offensive);
+        harrasment = (CheckBox) findViewById(R.id.reason_harrasment);
+        spam = (CheckBox) findViewById(R.id.reason_spam);
+        notAdvice = (CheckBox) findViewById(R.id.reason_notadvice);
+        joke = (CheckBox) findViewById(R.id.reason_joke);
+        offensive = (CheckBox) findViewById(R.id.reason_offensive);
         submitButton = (Button) findViewById(R.id.report_button);
 
+        if(harrasment.isChecked()){
 
+        }
+        if(spam.isChecked()){
+
+        }
+        if(notAdvice.isChecked()){
+
+        }
+        if(joke.isChecked()){
+
+        }
+        if(offensive.isChecked()){
+
+        }
+
+
+        submitButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+//                Intent i = new Intent(MainActivity.this, otherClass.class);
+//                i.putExtra("reason", reason);
+//                startActivity(i);
+            }
+        });
     }
+
 }
+
+
