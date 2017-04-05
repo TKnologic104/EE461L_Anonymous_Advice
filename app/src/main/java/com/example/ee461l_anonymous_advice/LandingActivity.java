95233eeae371775101ce08af356af395ce958971
Karime Saad <karime.saad@utexas.edu>
2017-04-05 02:46:41 -0500

    Fixed EditText from Landing page. Added character count and line doesn't move anymore

--- app/src/main/java/com/example/ee461l_anonymous_advice/LandingActivity.java
+++ app/src/main/java/com/example/ee461l_anonymous_advice/LandingActivity.java
@@ -3,9 +3,12 @@ package com.example.ee461l_anonymous_advice;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 //import com.example.tk_whatsappclone.R;
 
@@ -13,6 +16,7 @@ public class LandingActivity extends AppCompatActivity {
 
     private Button profile;
     private EditText problem;
+    private TextView charCount;
     private Button search;
     private Button friends;
     public String problemStatement;
@@ -26,6 +30,9 @@ public class LandingActivity extends AppCompatActivity {
         problem = (EditText)findViewById(R.id.problemText);
         search = (Button)findViewById(R.id.bn_search);
         friends = (Button)findViewById(R.id.bn_friends);
+        charCount = (TextView)findViewById(R.id.charCountValue);
+
+        problem.addTextChangedListener(mTextEditorWatcher);
 
 
 
@@ -52,5 +59,18 @@ public class LandingActivity extends AppCompatActivity {
     public void gotoFriends(){
         //TODO: create intent to goto Friends page
     }
+
+    private final TextWatcher mTextEditorWatcher = new TextWatcher() {
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        }
+
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            //This sets a textview to the current length
+            charCount.setText(String.valueOf(150-s.length()));
+        }
+
+        public void afterTextChanged(Editable s) {
+        }
+    };
 
 }
