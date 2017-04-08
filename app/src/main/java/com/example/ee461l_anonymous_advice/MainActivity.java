7a7684a86e3f401b11207eb65560c3a041d708b7
Tarang Khandpur <TKnologic104@gmail.com>
2017-04-08 18:19:34 -0500

    sign in and profile using firebase part 2

--- app/src/main/java/com/example/ee461l_anonymous_advice/MainActivity.java
+++ app/src/main/java/com/example/ee461l_anonymous_advice/MainActivity.java
@@ -5,7 +5,6 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
