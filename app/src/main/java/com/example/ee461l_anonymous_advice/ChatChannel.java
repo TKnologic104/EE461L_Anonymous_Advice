1d759627300fac121f55018d4b06fcb998667b53
Rodolfo-Gonzalez4335 <rodolfo.gonzalez4335@gmail.com>
2017-04-12 20:13:13 -0500

    Added user class and ChatChannel

---
+++ app/src/main/java/com/example/ee461l_anonymous_advice/ChatChannel.java
@@ -0,0 +1,38 @@
+package com.example.ee461l_anonymous_advice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by rodolfo on 4/12/2017.
+ */
+
+public class ChatChannel  {
+    User advisee;
+    User adviser;
+
+    String id;
+
+    Boolean isLocked;
+
+    List<Message> messages;
+
+    ChatChannel(String id, User advisee)
+    {
+        this.id=id;
+        this.advisee=advisee;
+        isLocked=false;
+        messages= new ArrayList<>();
+    }
+
+    public void addMessage(Message message)
+    {
+        messages.add(message);
+    }
+
+    public void addAdviser(User adviser)
+    {
+        this.adviser=adviser;
+        isLocked=true;
+    }
+}
